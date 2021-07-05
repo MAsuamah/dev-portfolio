473803c29e77df6b1de65d4367ceb33e750b658d
Michelle <michey.asmah21@gmail.com>
2021-07-05 17:50:27 -0400

    fade title

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import "../../assets/styles/Hero.css"
+import Fade from 'react-reveal/Fade';
 
 
 function Hero() {
   return (
     <div className="hero-image">
       <img src={require(`../../assets/images/portfolio-logo.png`).default}></img>
-      <h1>Full Stack Developer</h1>
+      <Fade delay={1000}>
+      <h1>Full Stack</h1>
+      </Fade>
+      <Fade delay={2000}>
+      <h1>Developer</h1>
+      </Fade>
     </div>
   );
 }
