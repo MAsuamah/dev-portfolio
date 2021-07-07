dfa219cbf7b194c5dff35091bac1974d57f2ed43
Michelle <michey.asmah21@gmail.com>
2021-07-07 18:07:49 -0400

    icon

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../../assets/styles/Portfolio.css'
-
+import { FaLink } from 'react-icons/fa';
+import { FaGithubSquare } from 'react-icons/fa';
 
 function Portfolio() {
   return (
@@ -15,32 +16,61 @@ function Portfolio() {
         <p id="author">-Émilie du Châtelet & Anthony J. D'Angelo</p>
       </div>
       <div className="portfolio">
+        
         <div class="grid">
-            <figure class="effect-oscar">
-              <img className="icon" src="https://tympanus.net/Development/HoverEffectIdeas/img/9.jpg" alt="img09"/>
-              <figcaption>
-                <h2>Warm <span>Oscar</span></h2>
-                <p>Oscar is a decent man. He used to clean porches with pleasure.</p>
-                <a href="#">View more</a>
-              </figcaption>     
-            </figure>
-            <figure class="effect-oscar">
-              <img className="icon" src="https://tympanus.net/Development/HoverEffectIdeas/img/10.jpg" alt="img10"/>
-              <figcaption>
-                <h2>Warm <span>Oscar</span></h2>
-                <p>Oscar is a decent man. He used to clean porches with pleasure.</p>
-                <a href="#">View more</a>
-              </figcaption>     
-            </figure>
-            <figure class="effect-oscar">
-              <img className="icon" src="https://tympanus.net/Development/HoverEffectIdeas/img/10.jpg" alt="img10"/>
-              <figcaption>
-                <h2>Warm <span>Oscar</span></h2>
-                <p>Oscar is a decent man. He used to clean porches with pleasure.</p>
-                <a href="#">View more</a>
-              </figcaption>     
-            </figure>
-          </div>
+          <figure class="effect-oscar">
+          <img className="icon" src={require(`../../assets/images/zengxiao-lin-fBZsl463vJQ-unsplash.jpg`).default}></img>
+            <figcaption>
+              <h2>Warm <span>Oscar</span></h2>
+              <p>Oscar is a decent man. He used to clean porches with pleasure.</p>
+              <a href="#">View more</a>
+            </figcaption>     
+          </figure>
+          <figure class="effect-oscar">
+            <img className="icon" src={require(`../../assets/images/ecommerce.png`).default}></img>
+            <figcaption>
+              <h2>Warm <span>Oscar</span></h2>
+              <p>Oscar is a decent man. He used to clean porches with pleasure.</p>
+              <a href="#">View more</a>
+            </figcaption>     
+          </figure>
+          <figure class="effect-oscar">
+            <img className="icon" src={require(`../../assets/images/AdobeStock_127928024.jpeg`).default}></img>
+            <figcaption>
+              <h2>Warm <span>Oscar</span></h2>
+              <p>Oscar is a decent man. He used to clean porches with pleasure.</p>
+              <a href="#">View more</a>
+            </figcaption>     
+          </figure>
+        </div>
+
+        <div class="grid">
+          <figure class="effect-oscar">
+          <img className="icon" src={require(`../../assets/images/catherine-hughes-PkEQHH6R7Eg-unsplash.jpg`).default}></img>
+            <figcaption>
+              <h2>Warm <span>Oscar</span></h2>
+              <p>Oscar is a decent man. He used to clean porches with pleasure.</p>
+              <a href="#">View more</a>
+            </figcaption>     
+          </figure>
+          <figure class="effect-oscar">
+          <img className="icon" src={require(`../../assets/images/budget.png`).default}></img>
+            <figcaption>
+              <h2>Warm <span>Oscar</span></h2>
+              <p>Oscar is a decent man. He used to clean porches with pleasure.</p>
+              <a href="#">View more</a>
+            </figcaption>     
+          </figure>
+          <figure class="effect-oscar">
+            <img className="icon" src={require(`../../assets/images/peinge-nakale-ie0YbSKO0fw-unsplash-copy.jpg`).default}></img>
+            <figcaption>
+              <h2>Warm <span>Oscar</span></h2>
+              <p>Oscar is a decent man. He used to clean porches with pleasure.</p>
+              <a href="#">View more</a>
+            </figcaption>     
+          </figure>
+        </div>
+          
       </div>
     </section>
   );
