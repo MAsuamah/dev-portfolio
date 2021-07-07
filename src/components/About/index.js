d79765006d3930216a7ebf7e8a44eee1f78c3f01
Michelle <michey.asmah21@gmail.com>
2021-07-07 15:51:48 -0400

    portfolio icons

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -54,7 +54,7 @@ function About() {
           <div className="inner-about">
             <img src={require(`../../assets/images/geo-port.png`).default}></img>
             <p> 
-              Hi, I’m Michelle! Here’s a bit about me…I am a highly self-motivated developer with an imminent certificate in full-stack web development. In previous studies I earned a Bachelor’s degree in Psychology and Human Resources, and my all-time favourite school subject is …math!
+              Here’s a bit about me…I am a highly self-motivated developer with an imminent certificate in full-stack web development. In previous studies I earned a Bachelor’s degree in Psychology and Human Resources, and my all-time favourite school subject is …math!
             </p>
             <p>
               I’ve always loved problem-solving and logical reasoning, and I've been searching for a way to introduce that back into my everyday life, especially my career. It took me a while, but I’m so happy I found coding because I feel it really fuels my passion for solving problems. I also consider myself a life-long learner, and I’m always eager to build and create. Hence, learning how to build web applications has been a fulfilling journey for me so far.
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,10 +1,47 @@
 import React from 'react';
+import '../../assets/styles/Portfolio.css'
+
 
 function Portfolio() {
   return (
     <section>
-      <h1>NAME</h1>
-      <p>Portfolio</p>
+      <div className="quote">
+        <h3>love of learning</h3>
+        <p>
+          is the most necessary passion . . . in it lies our happiness. <br />
+          It's a sure remedy for what ails us, an unending source of pleasure.
+          Develop a passion for learning. If you do, you will never cease to grow.
+        </p>
+        <p id="author">-Émilie du Châtelet & Anthony J. D'Angelo</p>
+      </div>
+      <div className="portfolio">
+        <div class="grid">
+            <figure class="effect-oscar">
+              <img src="https://tympanus.net/Development/HoverEffectIdeas/img/9.jpg" alt="img09"/>
+              <figcaption>
+                <h2>Warm <span>Oscar</span></h2>
+                <p>Oscar is a decent man. He used to clean porches with pleasure.</p>
+                <a href="#">View more</a>
+              </figcaption>     
+            </figure>
+            <figure class="effect-oscar">
+              <img src="https://tympanus.net/Development/HoverEffectIdeas/img/10.jpg" alt="img10"/>
+              <figcaption>
+                <h2>Warm <span>Oscar</span></h2>
+                <p>Oscar is a decent man. He used to clean porches with pleasure.</p>
+                <a href="#">View more</a>
+              </figcaption>     
+            </figure>
+            <figure class="effect-oscar">
+              <img src="https://tympanus.net/Development/HoverEffectIdeas/img/10.jpg" alt="img10"/>
+              <figcaption>
+                <h2>Warm <span>Oscar</span></h2>
+                <p>Oscar is a decent man. He used to clean porches with pleasure.</p>
+                <a href="#">View more</a>
+              </figcaption>     
+            </figure>
+          </div>
+      </div>
     </section>
   );
 }
