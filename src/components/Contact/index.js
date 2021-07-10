e4ff22accd81409726c8b485f24e08389726b848
Michelle <michey.asmah21@gmail.com>
2021-07-10 12:20:33 -0400

    error message for contact form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,10 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../../assets/styles/Contact.css'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 import Fade from 'react-reveal/Fade';
+import { validateEmail } from '../../utils/helpers';
 
 function Contact() {
+
+  const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+
+  const [errorMessage, setErrorMessage] = useState('');
+  const { name, email, message } = formState;
+
+  function handleChange(e) {
+    if (e.target.name === 'email') {
+      const isValid = validateEmail(e.target.value);
+      console.log(isValid);
+      if (!isValid) {
+        setErrorMessage('Your email is invalid.');
+      } else {
+        setErrorMessage('');
+      } 
+    } else {
+      if (!e.target.value.length) {
+        setErrorMessage(`${e.target.name} is required.`);
+      } else {
+        setErrorMessage('');
+      }
+    }
+    if (!errorMessage) {
+      setFormState({ ...formState, [e.target.name]: e.target.value });
+      console.log('Handle Form', formState);
+    }
+    if (!errorMessage) {
+      setFormState({ ...formState, [e.target.name]: e.target.value });
+    }
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(formState);
+  }
+
   return (
     <Fade>
       <section id="contact-container">
@@ -13,15 +50,29 @@ function Contact() {
             <h3 id="hi">Say Hi <span class="wave">👋</span></h3>
             <Form>
               <Form.Group controlId="exampleForm.ControlInput1">
-                <Form.Control type="email" placeholder="Name" />
+                <Form.Control type="name" placeholder="Name" 
+                name="Name" 
+                defaultValue={name} 
+                onBlur={handleChange}  />
               </Form.Group>
               <Form.Group controlId="exampleForm.ControlInput1">
-                <Form.Control type="email" placeholder="Email address" />
+                <Form.Control type="email" placeholder="Email address" 
+                name="email" 
+                defaultValue={email} 
+                onBlur={handleChange} />
               </Form.Group>
               <Form.Group controlId="exampleForm.ControlTextarea1">
-                <Form.Control as="textarea" rows={7} placeholder="Write Your Message" />
+                <Form.Control as="textarea" rows={7} placeholder="Write Your Message" 
+                name="Message" 
+                onBlur={handleChange}
+                defaultValue={message}  />
               </Form.Group>
-              <Button id="msg-btn"variant="warning" size="lg" >Submit</Button>{' '}
+              {errorMessage && (
+                <div>
+                  <p className="error-text">{errorMessage}</p>
+                </div>
+              )}
+              <Button id="msg-btn" >Submit</Button>{' '}   
             </Form>
           </div>
           <div id="right-contact" className="col-md-6">
@@ -31,7 +82,8 @@ function Contact() {
                 and allowing me to introduce myself.
                 <p id="contact-bolder">Now it's Your Turn!</p>
                 Send me a quick message or you can <br />
-                shoot me an email to the adresss below.
+                shoot me an email to the adresss below. <br />
+                <p id="contact-bolder">Let's talk about anything!</p>
               </p> <br />
               <a href="mailto:michey.asmah21@gmail.com">michey.asmah21@gmail.com</a>
             </div>
