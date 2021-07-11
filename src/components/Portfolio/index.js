06d23c1bd0e7407535a5491bb63cc4bcb00b3c6a
Michelle <michey.asmah21@gmail.com>
2021-07-11 19:52:14 -0400

    project component

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import '../../assets/styles/Portfolio.css'
-import { FaLink } from 'react-icons/fa';
-import { FaGithub } from 'react-icons/fa';
 import Fade from 'react-reveal/Fade';
+import Projects from "../Projects";
 
-function Portfolio() {
+function Portfolio({project}) {
   return (
-    <Fade delay={100}>
+    
+    <Fade >
       <section>
         {/* Quote*/}
         <div className="quote">
@@ -18,72 +18,77 @@ function Portfolio() {
           </p>
           <p id="author">-Émilie du Châtelet & Anthony J. D'Angelo</p>
         </div>
+
         {/* Portfolio*/}
+
         <div className="portfolio"> 
+
           <div className="grid">
-            <figure className="effect-oscar">
-            <img className="icon" src={require(`../../assets/images/zengxiao-lin-fBZsl463vJQ-unsplash.jpg`).default} alt="espresso cup resting on a macbook keyboard"></img>
-              <figcaption>
-                <h2>Tech <span>Cup</span> <br />
-                  <a href="https://tech-cup-blog.herokuapp.com/" target="_blank" rel="noreferrer"><FaLink /></a>
-                  <a href="https://github.com/MAsuamah/tech-cup-blog" target="_blank" rel="noreferrer"><FaGithub /></a>
-                </h2>
-                <p>MySQL | Express.js | Node.js </p>
-              </figcaption>     
-            </figure>
-            <figure className="effect-oscar">
-              <img className="icon" src={require(`../../assets/images/peinge-nakale-ie0YbSKO0fw-unsplash-copy.jpg`).default} alt="a tree in the sky. The tree features bright green foliage and the sky is clear with some fluffy white clouds."></img>
-              <figcaption>
-                <h2>Weather <span>App</span> <br />
-                  <a href="https://masuamah.github.io/weather-dashboard/" target="_blank" rel="noreferrer"><FaLink /></a>
-                  <a href="https://github.com/MAsuamah/weather-dashboard" target="_blank" rel="noreferrer"><FaGithub /></a>
-                </h2>
-                <p>JavaScript</p>
-              </figcaption>     
-            </figure>
-            <figure className="effect-oscar">
-              <img className="icon" src={require(`../../assets/images/AdobeStock_127928024.jpeg`).default} alt="a juicy double hamburger"></img>
-              <figcaption>
-                <h2>Super <span>Hip</span> <br />
-                  <a href="https://hmbrgr-hlpr.herokuapp.com/" target="_blank" rel="noreferrer"><FaLink /></a>
-                  <a href="https://github.com/fraudwheeldrive/Hmbrgr-Hlpr" target="_blank" rel="noreferrer"><FaGithub /></a>
-                </h2>
-                <p>MySQL | Express.js | Node.js</p>
-              </figcaption>     
-            </figure>
+
+              <Projects 
+                img={require(`../../assets/images/zengxiao-lin-fBZsl463vJQ-unsplash.jpg`).default}
+                alt= 'espresso cup resting on a macbook keyboard'
+                title='Tech'
+                span='Cup'
+                link='https://tech-cup-blog.herokuapp.com/'
+                github='https://github.com/MAsuamah/tech-cup-blog'
+                tech='MySQL | Express.js | Node.js'    
+              />
+              
+            <Projects 
+              img={require(`../../assets/images/peinge-nakale-ie0YbSKO0fw-unsplash-copy.jpg`).default}
+              alt= 'a tree in the sky. The tree features bright green foliage and the sky is clear with some fluffy white clouds.'
+              title='Weather'
+              span='App'
+              link='https://masuamah.github.io/weather-dashboard/'
+              github='https://github.com/MAsuamah/weather-dashboard'
+              tech='JavaScript'    
+            />
+     
+            <Projects 
+              img={require(`../../assets/images/AdobeStock_127928024.jpeg`).default}
+              alt= 'a juicy double hamburger'
+              title='Super'
+              span='Hip'
+              link='https://hmbrgr-hlpr.herokuapp.com/'
+              github='https://github.com/fraudwheeldrive/Hmbrgr-Hlpr'
+              tech='MySQL | Express.js | Node.js'    
+            />
+      
           </div>
 
           <div className="grid">
-            <figure className="effect-oscar">
-            <img className="icon" src={require(`../../assets/images/catherine-hughes-PkEQHH6R7Eg-unsplash.jpg`).default} alt="a white analog alarm clock"></img>
-              <figcaption>
-                <h2>Coding <span>Quiz</span> <br />
-                  <a href="https://masuamah.github.io/coding-quiz/ " target="_blank" rel="noreferrer"><FaLink /></a>
-                  <a href="https://github.com/MAsuamah/coding-quiz" target="_blank" rel="noreferrer"><FaGithub /></a>
-                </h2>
-                <p>JavaScript</p>
-              </figcaption>     
-            </figure>
-            <figure className="effect-oscar">
-            <img className="icon" src={require(`../../assets/images/budget.png`).default} alt="a graduation cap, a house, a piggybank, a shopping cart, a plane, and wedding rings resting on stacked coins"></img>
-              <figcaption>
-                <h2>Trackstar <span>Budget</span> <br />
-                  <a href="https://trackstar-budget.herokuapp.com/" target="_blank" rel="noreferrer"><FaLink /></a>
-                  <a href="https://github.com/MAsuamah/trackstar-budget" target="_blank" rel="noreferrer"><FaGithub /></a>
-                </h2>
-                <p>Progressive Web Application</p>
-              </figcaption>     
-            </figure>
-            <figure className="effect-oscar">
-              <img className="icon" src={require(`../../assets/images/ecommerce.png`).default} alt="a shopping cart reisting on a laptop keyboard"></img>
-              <figcaption>
-                <h2>Shop <span>Shop</span> <br />
-                  <a href="https://masuamah-shop-shop.herokuapp.com/" target="_blank" rel="noreferrer"><FaLink /></a>
-                  <a href="https://github.com/MAsuamah/shop-shop" target="_blank" rel="noreferrer"><FaGithub /></a>        
-                </h2>
-                <p>MongDB | Express.js. | Node.js | React.js</p>
-              </figcaption>     
-            </figure>
+
+            <Projects 
+              img={require(`../../assets/images/catherine-hughes-PkEQHH6R7Eg-unsplash.jpg`).default} 
+              alt= 'a white analog alarm clock"'
+              title='Coding'
+              span='Quiz'
+              link='https://masuamah.github.io/coding-quiz/'
+              github='https://github.com/MAsuamah/coding-quiz'
+              tech='JavaScript'    
+            />
+     
+            <Projects 
+              img={require(`../../assets/images/budget.png`).default} 
+              alt= 'a graduation cap, a house, a piggybank, a shopping cart, a plane, and wedding rings resting on stacked coins'
+              title='Trackstar'
+              span='Budget'
+              link='https://trackstar-budget.herokuapp.com/'
+              github='https://github.com/MAsuamah/trackstar-budget'
+              tech='Progressive Web Application'    
+            />
+           
+            <Projects 
+              img={require(`../../assets/images/ecommerce.png`).default}
+              alt= 'a shopping cart resting on a laptop keyboard'
+              title='Shop'
+              span='Shop'
+              link='https://masuamah-shop-shop.herokuapp.com/'
+              github='https://github.com/MAsuamah/shop-shop'
+              tech='MongDB | Express.js. | Node.js | React.js'    
+            />
+
           </div>  
         </div>
       </section>
---
+++ src/components/Projects/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { FaLink } from 'react-icons/fa';
+import { FaGithub } from 'react-icons/fa';
+
+function Projects(props) {
+    return (
+      <figure className="effect-oscar">
+      <img className="icon" src={props.img} alt={props.alt}></img>
+        <figcaption>
+          <h2>{props.title}<span>{props.span}</span> <br />
+            <a href={props.link} target="_blank" rel="noreferrer"><FaLink /></a>
+            <a href={props.github} target="_blank" rel="noreferrer"><FaGithub /></a>
+          </h2>
+          <p>{props.tech}</p>
+        </figcaption>     
+      </figure>
+    );
+}
+
+export default Projects;
