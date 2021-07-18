68a5f806a7b185c99d6cbd56a6f507d61cd7622c
Michelle <michey.asmah21@gmail.com>
2021-07-18 13:02:38 -0400

    add description for each project

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -22,10 +22,8 @@ function Portfolio() {
         {/* Portfolio*/}
 
         <div className="portfolio"> 
-
           <div className="grid">
-            
-
+    
               <Project
                 img={require(`../../assets/images/zengxiao-lin-fBZsl463vJQ-unsplash.jpg`).default}
                 alt= 'espresso cup resting on a macbook keyboard'
@@ -33,7 +31,8 @@ function Portfolio() {
                 span='Cup'
                 link='https://tech-cup-blog.herokuapp.com/'
                 github='https://github.com/MAsuamah/tech-cup-blog'
-                tech='MySQL | Express.js | Node.js'  
+                tech='MySQL | Express.js | Node.js'
+                description='A blog site where users can create a personal user account and post about tech. Tech Cup provides a community for those looking to interact with like-minded people.'  
               />
               
             <Project 
@@ -43,7 +42,8 @@ function Portfolio() {
               span='App'
               link='https://masuamah.github.io/weather-dashboard/'
               github='https://github.com/MAsuamah/weather-dashboard'
-              tech='JavaScript'    
+              tech='JavaScript'
+              description='A weather app that can find the forecast of any city. This weather app also keeps track of your search history for quicker searches in the future.'    
             />
      
             <Project 
@@ -53,7 +53,8 @@ function Portfolio() {
               span='Hip'
               link='https://hmbrgr-hlpr.herokuapp.com/'
               github='https://github.com/fraudwheeldrive/Hmbrgr-Hlpr'
-              tech='MySQL | Express.js | Node.js'    
+              tech='MySQL | Express.js | Node.js' 
+              description='A CMS restaurant app that helps owners manage the individual menus and locations of their chain restaurants from one app.'     
             />
       
           </div>
@@ -67,7 +68,8 @@ function Portfolio() {
               span='Quiz'
               link='https://masuamah.github.io/coding-quiz/'
               github='https://github.com/MAsuamah/coding-quiz'
-              tech='JavaScript'    
+              tech='JavaScript' 
+              description='A timed quiz that can keep track of high scores so you can compare your rank with your friends.'   
             />
      
             <Project
@@ -77,7 +79,8 @@ function Portfolio() {
               span='Budget'
               link='https://trackstar-budget.herokuapp.com/'
               github='https://github.com/MAsuamah/trackstar-budget'
-              tech='Progressive Web Application'    
+              tech='Progressive Web Application'
+              description='A budgeting app where you can track expenses and deposits. This app can be used offline and saved on mobile devices so you can keep track of your financials at any time.'   
             />
            
             <Project
@@ -87,7 +90,8 @@ function Portfolio() {
               span='Shop'
               link='https://masuamah-shop-shop.herokuapp.com/'
               github='https://github.com/MAsuamah/shop-shop'
-              tech='MongDB | Express.js. | Node.js | React.js'    
+              tech='MongDB | Express.js. | Node.js | React.js'
+              description='An E-commerce website that uses Global State and stripe to provide an easy, reliable and secure check out experience for customers.'    
             />
 
           </div>  
--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,15 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaLink } from 'react-icons/fa';
 import { FaGithub } from 'react-icons/fa';
+import { MdDescription } from 'react-icons/md';
+import Modal from 'react-bootstrap/Modal'
 
 function Project(props) {
+
+  const [smShow, setSmShow] = useState(false);
+
+  const descriptionBtn = {
+    color: "white",
+    backgroundColor: "transparent",
+    borderStyle: "none",
+    marginRight: "5px"
+  };
+
+  const spanStyle = {
+    marginLeft: "7px"
+  }
+
     return (
       <figure className="effect-oscar">
       <img className="icon" src={props.img} alt={props.alt}></img>
         <figcaption>
-          <h2 className="p-title">{props.title}<span>{props.span}</span> <br />
+          <h2 className="p-title">{props.title}<span style={spanStyle}>{props.span}</span> <br />
+          <div>
+            <button className="desc-modal" style={descriptionBtn} onClick={() => setSmShow(true)}><MdDescription/></button>
+            <Modal
+              size="sm"
+              show={smShow}
+              onHide={() => setSmShow(false)}
+              aria-labelledby="example-modal-sizes-title-sm"
+              class="dark"
+            >
+              <Modal.Header closeButton>
+                <Modal.Title id="example-modal-sizes-title-sm">
+                  {props.title} {props.span}
+                </Modal.Title>
+              </Modal.Header>
+              <Modal.Body>{props.description}</Modal.Body>
+            </Modal>
             <a href={props.link} target="_blank" rel="noreferrer"><FaLink /></a>
             <a href={props.github} target="_blank" rel="noreferrer"><FaGithub /></a>
+          </div>
           </h2>
           <p>{props.tech}</p>
         </figcaption>     
