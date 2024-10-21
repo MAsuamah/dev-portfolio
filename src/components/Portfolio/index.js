f54fa28b951d3754653a43253861d17ddb60bcba
Michelle Asuamah <michelleasuamah@Michelles-MBP-2.lan>
2024-10-21 11:47:39 -0400

    added python tech news

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -21,65 +21,29 @@ function Portfolio() {
         {/* Portfolio*/}
 
         <div className="portfolio"> 
+          
           <div className="grid">
 
-          <Project
-            img={require(`../../assets/images/mia-anderson-2k6v10Y2dIg-unsplash.jpg`).default} 
-            alt= 'two dogs playing'
-            title='Puppy'
-            span='Love'
-            link='https://puppy-love-date.herokuapp.com/'
-            github='https://github.com/MAsuamah/puppy-love'
-            tech='MongoDB| Express.js | React.js | Node.js'
-            description="Puppy Love is an app designed to find man's best friend a friend. Join the community of dog lovers online and set up doggy dates. This is a social app for pets where the user can sign their pets up and make a pet profile with images."   
-          />
-
-          <Project
-            img={require(`../../assets/images/markus-winkler-LNzuOK1GxRU-unsplash.jpg`).default}
-            alt= 'a typewriter printing out a sheet with the word goal on it'
-            title='Habit'
-            span='Tracker'
-            link='https://peterw712.github.io/habit-tracker/'
-            github='https://github.com/peterw712/habit-tracker'
-            tech='JavaScript | Bootstrap'
-            description='Habit Tracker is a way to hold yourself accountable for good habits every day. Enter your habits and give yourself a score to represent how much effort you put in. Also keep an eye on your 30 day record to see your progress.'    
-          />
-    
-            <Project 
-              img={require(`../../assets/images/AdobeStock_127928024.jpeg`).default}
-              alt= 'a juicy double hamburger'
-              title='Super'
-              span='Hip'
-              link='https://hmbrgr-hlpr.herokuapp.com/'
-              github='https://github.com/fraudwheeldrive/Hmbrgr-Hlpr'
-              tech='MySQL | Express.js | Node.js' 
-              description='A CMS restaurant app that helps owners manage the individual menus and locations of their chain restaurants from one app.'     
+            <Project
+              img={require(`../../assets/images/zengxiao-lin-fBZsl463vJQ-unsplash.jpg`).default}
+              alt= 'espresso cup resting on a macbook keyboard'
+              title='Tech'
+              span='Cup'
+              link='https://tech-cup-blog.herokuapp.com/'
+              github='https://github.com/MAsuamah/tech-cup-blog'
+              tech='MySQL | Express.js | Node.js'
+              description='A blog site where users can create a personal user account and post about tech. Tech Cup provides a community for those looking to interact with like-minded people.'  
             />
-      
-          </div>
-
-          <div className="grid">
-
-          <Project
-                img={require(`../../assets/images/zengxiao-lin-fBZsl463vJQ-unsplash.jpg`).default}
-                alt= 'espresso cup resting on a macbook keyboard'
-                title='Tech'
-                span='Cup'
-                link='https://tech-cup-blog.herokuapp.com/'
-                github='https://github.com/MAsuamah/tech-cup-blog'
-                tech='MySQL | Express.js | Node.js'
-                description='A blog site where users can create a personal user account and post about tech. Tech Cup provides a community for those looking to interact with like-minded people.'  
-              />
               
             <Project 
-              img={require(`../../assets/images/peinge-nakale-ie0YbSKO0fw-unsplash-copy.jpg`).default}
-              alt= 'a tree in the sky. The tree features bright green foliage and the sky is clear with some fluffy white clouds.'
-              title='Weather'
-              span='App'
-              link='https://masuamah.github.io/weather-dashboard/'
-              github='https://github.com/MAsuamah/weather-dashboard'
-              tech='JavaScript'
-              description='A weather app that can find the forecast of any city. This weather app also keeps track of your search history for quicker searches in the future.'    
+              img={require(`../../assets/images/news.jpg`).default}
+              alt= 'a man reading a business news paper.'
+              title='Just-Tech'
+              span='News'
+              link='https://masuamah-python-newsfeed.herokuapp.com/'
+              github='https://github.com/MAsuamah/python-newsfeed'
+              tech='Python'
+              description='Tech news website where users can post, upvote, and comment on links to news articles about tech.'    
             />     
 
             <Project 
@@ -97,38 +61,38 @@ function Portfolio() {
 
           <div className="grid">
 
-          <Project
-              img={require(`../../assets/images/budget.png`).default} 
-              alt= 'a graduation cap, a house, a piggybank, a shopping cart, a plane, and wedding rings resting on stacked coins'
-              title='Trackstar'
-              span='Budget'
-              link='https://trackstar-budget.herokuapp.com/'
-              github='https://github.com/MAsuamah/trackstar-budget'
-              tech='Progressive Web Application'
-              description='A budgeting app where you can track expenses and deposits. This app can be used offline and saved on mobile devices so you can keep track of your financials at any time.'   
-            />
-           
             <Project
-              img={require(`../../assets/images/ecommerce.png`).default}
-              alt= 'a shopping cart resting on a laptop keyboard'
-              title='Shop'
-              span='Shop'
-              link='https://masuamah-shop-shop-redux.herokuapp.com/'
-              github='https://github.com/MAsuamah/shop-shop-redux'
-              tech='MERN Stack | Redux | Stripe'
-              description='An E-commerce website that uses Redux to handle Global State and stripe to provide an easy, reliable and secure check out experience for customers.'    
-            />
+                img={require(`../../assets/images/budget.png`).default} 
+                alt= 'a graduation cap, a house, a piggybank, a shopping cart, a plane, and wedding rings resting on stacked coins'
+                title='Trackstar'
+                span='Budget'
+                link='https://trackstar-budget.herokuapp.com/'
+                github='https://github.com/MAsuamah/trackstar-budget'
+                tech='Progressive Web Application'
+                description='A budgeting app where you can track expenses and deposits. This app can be used offline and saved on mobile devices so you can keep track of your financials at any time.'   
+              />
+            
+              <Project
+                img={require(`../../assets/images/ecommerce.png`).default}
+                alt= 'a shopping cart resting on a laptop keyboard'
+                title='Shop'
+                span='Shop'
+                link='https://masuamah-shop-shop-redux.herokuapp.com/'
+                github='https://github.com/MAsuamah/shop-shop-redux'
+                tech='MERN Stack | Redux | Stripe'
+                description='An E-commerce website that uses Redux to handle Global State and stripe to provide an easy, reliable and secure check out experience for customers.'    
+              />
 
-          <Project 
-            img={require(`../../assets/images/catherine-hughes-PkEQHH6R7Eg-unsplash.jpg`).default} 
-            alt= 'a white analog alarm clock"'
-            title='Coding'
-            span='Quiz'
-            link='https://masuamah.github.io/coding-quiz/'
-            github='https://github.com/MAsuamah/coding-quiz'
-            tech='JavaScript' 
-            description='A timed quiz that can keep track of high scores so you can compare your rank with your friends.'   
-          />
+            <Project 
+              img={require(`../../assets/images/catherine-hughes-PkEQHH6R7Eg-unsplash.jpg`).default} 
+              alt= 'a white analog alarm clock"'
+              title='Coding'
+              span='Quiz'
+              link='https://masuamah.github.io/coding-quiz/'
+              github='https://github.com/MAsuamah/coding-quiz'
+              tech='JavaScript' 
+              description='A timed quiz that can keep track of high scores so you can compare your rank with your friends.'   
+            />
 
           </div> 
         </div>
